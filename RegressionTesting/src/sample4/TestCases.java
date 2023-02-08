b138f164b3a8aef3e95dca3ea37034bc327a1dba
AdityaSawant96 <adiwant996@gmail.com>
2023-02-08 14:45:52 +0530

    Secont commit for changes in Regression Suite

---
+++ RegressionTesting/src/sample4/TestCases.java
@@ -0,0 +1,25 @@
+package sample4;
+
+public class TestCases {
+
+	public static void main(String[] args) {
+		TestCases r = new TestCases();
+		r.test1();
+		r.test2();
+		r.test3();
+	}
+	public void test1()
+	{
+		System.out.println(10+5);
+	}
+	public void test2()
+	{
+		System.out.println(10-5);
+	}
+	public void test3()
+	{
+		System.out.println(15*3);
+	}
+	
+
+}
